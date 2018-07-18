8e14dfbca285fd012b9416c351a71fec6b738f67
Samuel Ludwig <samuelludwig74@yahoo.com>
2018-07-18 10:42:26 -0400

    fixed <?> being everywhere, will need to make a second pass

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 /* Course Outlines button functions */
 
 function show500() {
-    $("#doc-port").load("https://samuelludwig.github.io//AST101//Course_Outlines//banks.txt");
-    // document.getElementById("doc-port").innerHTML = txt;
+    $("#doc-port").load("https://samuelludwig.github.io//AST101//Course_Outlines//500.txt");
 }
 
 function show501() {
-    document.getElementById("doc-port").innerHTML = 501;
+    $("#doc-port").load("https://samuelludwig.github.io//AST101//Course_Outlines//501.txt");
 }
 
 function show504() {
-    document.getElementById("doc-port").innerHTML = 504;
+    $("#doc-port").load("https://samuelludwig.github.io//AST101//Course_Outlines//504.txt");
 }
